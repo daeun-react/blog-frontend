9ecb4ff57a99c567c0abf9b4c5c7e241496430a3
이다은 (Da Eun Lee) <daeun.lee@hrc.co.kr>
2020-07-23 16:40:03 +0900

    first commit

---
+++ src/pages/RegisterPage.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function RegisterPage() {
+  return <div>회원가입</div>;
+}
+
+export default RegisterPage;
---
+++ src/pages/WritePage.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function WritePage() {
+  return <div>글쓰기</div>;
+}
+
+export default WritePage;
